e6d2e6bcf89c65218a82ffcdfa74293cde3b7519
lvivduncan <lvivduncan@gmail.com>
2021-04-13 16:52:24 +0300

    page.html

--- js/fares.js
+++ js/fares.js
@@ -1,9 +1,9 @@
 
 // клік на групу тарифів
-$('.fares-tabs a').on('click',function(e){
+$('#fares dt a').on('click',function(e){
     e.preventDefault();
 
-    $('.fares-tabs a').removeClass('active');
+    $('#fares dt a').removeClass('active');
 
     $(this).addClass('active');
 });
