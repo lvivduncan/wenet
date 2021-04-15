df2bc1602e392786a0c0824d5314ca566f73bd50
duncan <lvivduncan@gmail.com>
2021-04-15 13:07:15 +0300

    #slider

--- js/init.js
+++ js/init.js
@@ -15,3 +15,23 @@ $('.levus-dropdown-wrapper').on('click', function(){
 // плагін дропдауна
 
 
+/* 
+$('#nav-panel-city');
+
+$('#nav-panel-cabinet');
+
+$('#nav-panel-contacts');
+ */
+
+
+
+// slider home page
+$('#slider .owl-carousel').owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true
+});
