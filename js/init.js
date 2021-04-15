e162725fd98c08104e2d9a8cb893ab92b8b0ad46
duncan <lvivduncan@gmail.com>
2021-04-15 15:38:42 +0300

    .phones, .shedule

--- js/init.js
+++ js/init.js
@@ -2,8 +2,7 @@
 // плагін дропдауна
 $('body').append('<div class="levus-dropdown-wrapper"></div>');
 
-$('.levus-dropdown a').on('click', function(e){
-    e.preventDefault();
+$('.nav-panel').on('click', function(){
     $(this).next('.levus-dropdown-content').toggleClass('open');
     $('.levus-dropdown-wrapper').toggleClass('open');
 });
@@ -14,6 +13,42 @@ $('.levus-dropdown-wrapper').on('click', function(){
 });
 // плагін дропдауна
 
+
+// зв'язка місто + контакти
+$('.cities li').on('click', function(e){
+    var city = e.target.textContent;
+
+    // міняємо назву міста
+    $('#city').text(city);
+    $('#nav-panel-contacts .levus-dropdown-content h5:first-child').text(city);
+
+    // закриваємо випадачку
+    $(this).parents('.levus-dropdown-content').removeClass('open');
+
+    // відкриваємо випадачку з контактами
+    $('#nav-panel-contacts .levus-dropdown-content').addClass('open');
+
+    // перемикаємо телефони
+    if(city != 'Борислав'){
+        // phones
+        $('.levus-dropdown-content .phones').eq(0).removeClass('open');
+        $('.levus-dropdown-content .phones').eq(1).addClass('open');
+        
+        // shedule     
+        $('.levus-dropdown-content .shedule').eq(0).removeClass('open');
+        $('.levus-dropdown-content .shedule').eq(1).addClass('open');
+    } else {
+        // phones
+        $('.levus-dropdown-content .phones').eq(0).addClass('open');
+        $('.levus-dropdown-content .phones').eq(1).removeClass('open');
+
+        // shedule
+        $('.levus-dropdown-content .shedule').eq(0).addClass('open');
+        $('.levus-dropdown-content .shedule').eq(1).removeClass('open');
+    }
+
+});
+// зв'язка місто + контакти
 
 /* 
 $('#nav-panel-city');
